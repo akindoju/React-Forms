f87af2ad3f442c9253a307f420fa2120dbf12295
Akndju <davidd.akindoju@gmail.com>
2021-03-23 14:26:15 +0100

    Created validation function for each field

--- src/standardForm/standardForm.js
+++ src/standardForm/standardForm.js
@@ -1,6 +1,84 @@
 import React, { Component } from 'react';
 
 class StandardForm extends Component {
+  state = {
+    username: '',
+    usernameValid: false,
+    email: '',
+    emailValid: false,
+    password: '',
+    passwordValid: '',
+    passwordConfirm: '',
+    passwordConfirmValid: false,
+    formValid: false,
+    errorMsg: {},
+  };
+
+  validateUsername = () => {
+    const { username } = this.state;
+    let usernameValid = true;
+    let errorMsg = { ...this.state.errorMsg };
+
+    if (username.length < 6 || username.length > 15) {
+      usernameValid = false;
+      errorMsg.username = 'Username should be between 6 and 15 characters';
+    }
+
+    this.setState({ errorMsg, usernameValid }, this.validateForm);
+  };
+
+  validateEmail = () => {
+    const { email } = this.state;
+    let emailValid = true;
+    let errorMsg = { ...this.state.errorMsg };
+
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+      emailValid = false;
+      errorMsg.email = 'Invalid Email Address';
+    }
+
+    this.setState({ errorMsg, emailValid }, this.validateForm);
+  };
+
+  validatepassword = () => {
+    const { password } = this.state;
+    let passwordValid = true;
+    let errorMsg = { ...this.state.errorMsg };
+
+    if (password.length < 6) {
+      passwordValid = false;
+      errorMsg.password = 'Password should be at least 6 characters';
+    }
+
+    this.setState({ errorMsg, passwordValid }, this.validateForm);
+  };
+
+  validateConfirmPassword = () => {
+    const { passwordConfirm, password } = this.state;
+    let passwordConfirmValid = true;
+    let errorMsg = { ...this.state.errorMsg };
+
+    if (password !== passwordConfirm) {
+      passwordConfirmValid = false;
+      errorMsg.passwordConfirm = 'Passwords do not match';
+    }
+
+    this.setState({ passwordConfirmValid, errorMsg }, this.validateForm);
+  };
+
+  validateForm = () => {
+    const {
+      usernameValid,
+      passwordConfirmValid,
+      emailValid,
+      passwordValid,
+    } = this.state;
+    this.setState({
+      formValid:
+        usernameValid && emailValid && passwordValid && passwordConfirmValid,
+    });
+  };
+
   render() {
     return (
       <div>
@@ -8,17 +86,35 @@ class StandardForm extends Component {
         <form>
           <div className="form-group">
             <label htmlFor="username">Username</label>
-            <input type="text" id="username" className="form-control" />
+            <input
+              type="text"
+              id="username"
+              className="form-control"
+              value={this.state.username}
+              onChange={(e) => this.setState({ username: e.target.value })}
+            />
           </div>
 
           <div className="form-group">
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" className="form-control" />
+            <input
+              type="email"
+              id="email"
+              className="form-control"
+              value={this.state.email}
+              onChange={(e) => this.setState({ email: e.target.value })}
+            />
           </div>
 
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" className="form-control" />
+            <input
+              type="password"
+              id="password"
+              className="form-control"
+              value={this.state.password}
+              onChange={(e) => this.setState({ password: e.target.value })}
+            />
           </div>
 
           <div className="form-group">
@@ -27,6 +123,10 @@ class StandardForm extends Component {
               type="password"
               id="confirmPassword"
               className="form-control"
+              value={this.state.passwordConfirm}
+              onChange={(e) =>
+                this.setState({ passwordConfirm: e.target.value })
+              }
             />
           </div>
         </form>
